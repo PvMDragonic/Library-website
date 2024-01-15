93f08dba65172dd8c6f696342a27904181b67729
PvMDragonic <dragonicpvm@gmail.com>
2024-01-15 15:43:02 -0300

    Fixing <NavBar> buttons' spacing.
    Also changed the JSX a bit, making it more readable and semantic.

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,13 +1,21 @@
 export function NavBar()
 {
     return (
-        <div className="navbar">
-            <div className="navbar__title">Library</div>
-                <div className="navbar__links">
+        <nav className="navbar" role="navigation" aria-label="Main">
+            <h1 className="navbar__title">
+                Library
+            </h1>
+            <ul className="navbar__links">
+                <li className="navbar__item">
                     <a href='/' className="navbar__button">Home</a>
+                </li>
+                <li className="navbar__item">
                     <a href='/new' className="navbar__button">New Book</a>
+                </li>
+                <li className="navbar__item">
                     <a href='/tags' className="navbar__button">Edit Tags</a>
-                </div>
-        </div>
+                </li>
+            </ul>
+        </nav>
     )
 }
