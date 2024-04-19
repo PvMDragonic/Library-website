cd5e0955037b339e1f2b6649e939704e60eb55cd
PvMDragonic <dragonicpvm@gmail.com>
2024-04-19 14:54:51 -0300

    <XContainer> text scroll when too big.
    Also decided to standardiz the srolling via a <Scrollable> component,
    since alot of components use thigs scrolling-text thing.

---
+++ frontend/src/components/Scrollable/index.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+interface IScrollable
+{
+    scrollingText: React.RefObject<HTMLSpanElement>;
+    parentDiv: React.RefObject<HTMLDivElement>;
+    offset?: number; // How much space on the right.
+}
+
+export function Scrollable({ scrollingText, parentDiv, offset = 5 }: IScrollable)
+{
+    const [shouldScroll, setShouldScroll] = useState<boolean>(false);
+
+    useEffect(() => 
+    {
+        const parentRef = parentDiv.current;
+        const textRef = scrollingText.current;
+        if (!parentRef || !textRef) return;
+
+        const resizeObserver = new ResizeObserver(() => 
+        {
+            const parentWidth = parentRef.offsetWidth;
+            const textWidth = textRef.offsetWidth;
+
+            const widthDiff = (textWidth - parentWidth) + 10;
+            const tooBig = textWidth > (parentWidth - offset);
+
+            if (tooBig)
+            {
+                const animDistance = (widthDiff * -1) - (offset - 5);
+                const animDuration = animDistance * -3 / 10;
+            
+                textRef.style.setProperty('--scroll-duration', `${animDuration}s`);
+                textRef.style.setProperty('--scroll-distance', `${animDistance}px`);
+            }
+
+            setShouldScroll(tooBig);
+        });
+
+        resizeObserver.observe(parentRef);
+        resizeObserver.observe(textRef);
+
+        return () => resizeObserver.disconnect();        
+    }, []);
+
+    return ({ shouldScroll })
+}
--- frontend/src/components/XContainer/index.tsx
+++ frontend/src/components/XContainer/index.tsx
@@ -1,5 +1,6 @@
-import { useMemo } from "react";
+import { useMemo, useEffect, useRef, useState } from "react";
 import { isDarkColor } from "../../utils/color";
+import { Scrollable } from "../Scrollable";
 
 interface IXContainer
 {
@@ -12,18 +13,45 @@ const veryLightGrey = '#82D1D3';
 
 export function XContainer({ text, color, onClick }: IXContainer)
 {
+    const [divClass, setDivClass] = useState<string>('');
+    const [textClass, setTextClass] = useState<string>('');
+
+    const parentDivRef = useRef<HTMLDivElement>(null);
+    const scrollingTextRef = useRef<HTMLSpanElement>(null);
+
     const colorScheme = useMemo(() => (
         isDarkColor(color || veryLightGrey)
     ), []);
 
+    const { shouldScroll } = Scrollable({
+        scrollingText: scrollingTextRef,
+        parentDiv: parentDivRef,
+        offset: 20 // Account for the 'x' button.
+    })
+
+    useEffect(() =>
+    {
+        const colorClass = `XContainer--${colorScheme ? 'white' : 'black'}`;
+        const scrollClass = `XContainer--${shouldScroll ? 'too-big' : 'regular'}`;
+        setDivClass(`XContainer ${colorClass} ${scrollClass}`);
+        setTextClass(`XContainer__text${shouldScroll ? ' XContainer__text--too-big' : ''}`);
+    }, [shouldScroll]);
+
     return (
         <div 
-            className = {`XContainer XContainer--${colorScheme ? 'white' : 'black'}`}
+            ref = {parentDivRef}
+            className = {divClass}
             style = {{ backgroundColor: color || veryLightGrey }}
         >
-            {text}
+            <span 
+                ref = {scrollingTextRef}
+                className = {textClass}
+            >
+                {text}
+            </span>
             <span 
                 className = {`XContainer__close XContainer__close--${colorScheme ? 'white' : 'black'}`}
+                style = {{ backgroundColor: color || veryLightGrey }}
                 onClick = {onClick}
             >
                 🗙
