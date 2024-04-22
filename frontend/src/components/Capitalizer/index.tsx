30943f1623fd2ba27a21759170779715e7657880
PvMDragonic <dragonicpvm@gmail.com>
2024-04-22 12:27:28 -0300

    <Capitalizer> component for <XContainer> formatting.
    Uppercase letters look better, but forcing it makes the <SearchBar> case
    filter useless and also removes the user's option of having it one way or
    the other. This way, every letter will be uppercase, but upper- and
    lower-case letters have different sizes to tell them apart.

---
+++ frontend/src/components/Capitalizer/index.tsx
@@ -0,0 +1,49 @@
+import { useMemo } from "react";
+
+interface ICapitalizer
+{
+    text: string;
+}
+
+function isLetter(char: string) 
+{
+    // Captures letters, including those with accentuation such as ã, é, ó, etc.
+    return /^\p{L}$/u.test(char);
+}
+
+function isCapital(char: string)
+{
+    return /^[A-Z]$/.test(char);
+}
+
+export function Capitalizer({ text }: ICapitalizer)
+{
+    const capitalizedText = useMemo(() => 
+    {
+        return text.split(' ').map((word, wordIndex, wordArray) => 
+        {
+            return word.split('').map((char, charIndex, charArray) => 
+            {
+                const isLastChar = charIndex === charArray.length - 1;
+                const addTrailingSpace = wordIndex !== wordArray.length - 1 && isLastChar;
+
+                const style = {
+                    fontWeight: 100,
+                    fontSize: isCapital(char) || !isLetter(char)
+                        ? '16px' : '13px'
+                }
+
+                return (
+                    <span 
+                        key = {`${char}${wordIndex}${charIndex}`}
+                        style = {style}
+                    >
+                        {addTrailingSpace ? `${char} ` : char}
+                    </span>
+                );
+            });
+        });
+    }, [text]);
+
+    return <>{capitalizedText}</>;
+}
