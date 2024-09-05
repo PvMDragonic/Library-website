bbab8ca683312b301784504818c92b416d72f2c2
PvMDragonic <dragonicpvm@gmail.com>
2024-09-05 15:20:45 -0300

    Adding 'useScrollable' to <TitleContainer>.
    Had to change the <h1>'s structure inside <TitleContainer>, because
    the way it was before would make so that 'useScrollable' wouldn't get a
    textRef when it was defined, leading to it always being false. Now,
    either way the <h1> will always get the useRef.
    
    This took me way longer for me to notice why it was broken than I'd like
    to admit, but at least it looks cleaner now.

--- frontend/src/components/TitleContainer/index.tsx
+++ frontend/src/components/TitleContainer/index.tsx
@@ -1,4 +1,5 @@
 import { useRef, useEffect, useState } from 'react';
+import { useScrollable } from '../../hooks/useScrollable';
 import { SearchType } from '../../pages/Home';
 
 interface ITitleContainer
@@ -9,62 +10,34 @@ interface ITitleContainer
 
 export function TitleContainer({ totalBooks, searchOption }: ITitleContainer)
 {
-    const [parentDivSize, setParentDivSize] = useState<number>(0);
-    const [textClass, setTextClass] = useState<string>();
-
     const parentDivRef = useRef<HTMLDivElement>(null);
     const titleTextRef = useRef<HTMLHeadingElement>(null);
     
-    useEffect(() => 
-    {
-        const parentDiv = parentDivRef.current;
-        if (!parentDiv) return;
+    const { shouldScroll } = useScrollable({
+        scrollingText: titleTextRef,
+        parentDiv: parentDivRef
+    });
 
-        const resizeObserver = new ResizeObserver(
-            () => setParentDivSize(parentDiv.offsetWidth)
-        );
-    
-        resizeObserver.observe(parentDiv);
-
-        return () => resizeObserver.unobserve(parentDiv);
-    }, []);
-
-    useEffect(() =>
-    {
-        const titleText = titleTextRef.current;
-        if (!titleText) return;
-
-        const widthDiff = (titleText.offsetWidth - parentDivSize) + 10;
-        const animDistance = widthDiff > 10 ? widthDiff * -1 : -10;
-        const animDuration = animDistance * -3 / 10;
-
-        titleText.style.setProperty('--scroll-duration', `${animDuration}s`);
-        titleText.style.setProperty('--scroll-distance', `${animDistance}px`);
-
-        setTextClass(titleText.offsetWidth > (parentDivSize - 5) 
-            ? 'main-title__header main-title__header--scroll' 
-            : 'main-title__header'
-        );
-    }, [parentDivSize, searchOption]);
+    const textClass = shouldScroll 
+        ? 'main-title__header main-title__header--scroll' 
+        : 'main-title__header';
 
     return (
         <div className = "main-title">
             <div className = "main-title__header-container" ref = {parentDivRef}>
-                {searchOption && searchOption.type !== '' ? (
-                    <h1 className = {textClass} ref = {titleTextRef}>
-                        {searchOption.value === '' ? (
+                <h1 className = {textClass} ref = {titleTextRef}>
+                    {searchOption && searchOption.type !== '' ? (
+                        searchOption.value === '' ? (
                             <span>
                                 {searchOption.type}: <i>Unknown</i>
                             </span>
                         ) : (
                             `${searchOption.type}: "${searchOption.value}"`
-                        )}
-                    </h1>
-                ) : (
-                    <h1 className = "main-title__header">
-                        My Books
-                    </h1>
-                )}
+                        )
+                    ) : (
+                        'My Books'
+                    )}
+                </h1>
             </div>
             <span className = "main-title__total-books">
                 Total Books: {totalBooks}
