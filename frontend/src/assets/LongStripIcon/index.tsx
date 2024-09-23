64f28b07efb713a57612f9f7cb5670f2542cc3bf
PvMDragonic <dragonicpvm@gmail.com>
2024-09-23 17:38:06 -0300

    More PDF reader control options.

---
+++ frontend/src/assets/LongStripIcon/index.tsx
@@ -0,0 +1,20 @@
+function LongStripIcon()
+{
+    return (
+        <svg 
+            xmlns = "http://www.w3.org/2000/svg"
+            viewBox = "0 0 24 24" 
+            fill = "none" 
+        >
+            <path 
+                stroke = "currentColor" 
+                strokeLinecap = "round" 
+                strokeLinejoin = "round" 
+                strokeWidth = "2" 
+                d = "M20 2v6a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V2m0 20v-6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v6"
+            />
+        </svg>
+    )
+}
+
+export default LongStripIcon;
