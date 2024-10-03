95d2927113d04b8ff1a0a5d5633565b855b21ebb
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2024-10-03 14:27:22 -0300

    PDF reader mobile responsiveness.
    The <ControlPanel> will split in two rows below 500px.

--- frontend/src/components/ControlPanel/index.tsx
+++ frontend/src/components/ControlPanel/index.tsx
@@ -128,80 +128,84 @@ export function ControlPanel({
             style = {{
                 // Added dynamically because it'd clip the very top of the first 
                 // page when the scrollbar is at the very top, which looked weird.
-                ...(sectionScrolled && { boxShadow: '0rem 1rem 0.5rem rgb(255, 255, 255)' }),
+                ...(sectionScrolled && { boxShadow: '0rem 1rem 0.5rem 0.05rem rgb(255, 255, 255)' }),
                 marginRight: hasScroll ? '3.5rem' : '2rem' 
             }}
         >
-            <button
-                type = "button"
-                title = "Previous page"
-                className = "control-pannel__button"
-                disabled = {pageNumber <= 1}
-                onClick = {() => handlePageSelect(-1)}
-            >
-                <ArrowLeftIcon/>
-            </button>
-            <span>
-                Page 
-                <input
-                    type = "number"
-                    name = "pageNumber"
-                    className = "control-pannel__input"
-                    onChange = {(e) => handlePageSelect(Number(e.target.value))}
-                    value = {pageNumber}
-                    max = {numPages || 1}
-                    min = {1}
-                /> 
-                of {numPages}
-            </span>
-            <button
-                type = "button"
-                title = "Next page"
-                className = "control-pannel__button"
-                disabled = {pageNumber >= numPages}
-                onClick = {() => handlePageSelect(+1)}
-            >
-                <ArrowRightIcon/>
-            </button>
-            <button
-                type = "button"
-                title = "Zoom in"
-                className = "control-pannel__button"
-                disabled = {scale >= 2.0}
-                onClick = {() => setScale(scale + 0.1)}
-            >
-                <ZoomInIcon/>
-            </button>
-            <span>{(scale * 100).toFixed()}%</span>
-            <button
-                type = "button"
-                title = "Zoom out"
-                className = "control-pannel__button"
-                disabled = {scale < 0.6}
-                onClick = {() => setScale(scale - 0.1)}
-            >
-                <ZoomOutIcon/>
-            </button>
-            <button
-                type = "button"
-                title = {`Display mode — ${singlePage ? 'Single-page mode' : 'Long-strip mode'}`}
-                className = "control-pannel__button"
-                onClick = {handlePageMode}
-            >
-                {singlePage ? (
-                    <SinglePageIcon/>
-                ) : (
-                    <LongStripIcon/>
-                )}
-            </button>
-            <button
-                type = "button"
-                title = {`Scroll mode — ${scrollDescriptions[scrollMode]}`}
-                className = "control-pannel__button"
-                onClick = {handleScrollMode}
-            >
-                {scrollButtons[scrollMode]}
-            </button>
+            <div className = "control-pannel__container">
+                <button
+                    type = "button"
+                    title = "Previous page"
+                    className = "control-pannel__button"
+                    disabled = {pageNumber <= 1}
+                    onClick = {() => handlePageSelect(-1)}
+                >
+                    <ArrowLeftIcon/>
+                </button>
+                <span>
+                    Page 
+                    <input
+                        type = "number"
+                        name = "pageNumber"
+                        className = "control-pannel__input"
+                        onChange = {(e) => handlePageSelect(Number(e.target.value))}
+                        value = {pageNumber}
+                        max = {numPages || 1}
+                        min = {1}
+                    /> 
+                    of {numPages}
+                </span>
+                <button
+                    type = "button"
+                    title = "Next page"
+                    className = "control-pannel__button"
+                    disabled = {pageNumber >= numPages}
+                    onClick = {() => handlePageSelect(+1)}
+                >
+                    <ArrowRightIcon/>
+                </button>
+            </div>
+            <div className = "control-pannel__container">
+                <button
+                    type = "button"
+                    title = "Zoom in"
+                    className = "control-pannel__button"
+                    disabled = {scale >= 2.0}
+                    onClick = {() => setScale(scale + 0.1)}
+                >
+                    <ZoomInIcon/>
+                </button>
+                <span>{(scale * 100).toFixed()}%</span>
+                <button
+                    type = "button"
+                    title = "Zoom out"
+                    className = "control-pannel__button"
+                    disabled = {scale < 0.6}
+                    onClick = {() => setScale(scale - 0.1)}
+                >
+                    <ZoomOutIcon/>
+                </button>
+                <button
+                    type = "button"
+                    title = {`Display mode — ${singlePage ? 'Single-page mode' : 'Long-strip mode'}`}
+                    className = "control-pannel__button"
+                    onClick = {handlePageMode}
+                >
+                    {singlePage ? (
+                        <SinglePageIcon/>
+                    ) : (
+                        <LongStripIcon/>
+                    )}
+                </button>
+                <button
+                    type = "button"
+                    title = {`Scroll mode — ${scrollDescriptions[scrollMode]}`}
+                    className = "control-pannel__button"
+                    onClick = {handleScrollMode}
+                >
+                    {scrollButtons[scrollMode]}
+                </button>
+            </div>
         </div>
     )
 }
